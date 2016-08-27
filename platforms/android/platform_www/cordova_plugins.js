9d2d9453b0fba9cba95ee853a1548619bc2e5a16
sungjp <gpark18@amherst.edu>
2016-08-26 21:39:03 -0400

    added unstable plugin for audio recording, ceasing addition of platform files

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-audioinput/www/audioInputCapture.js",
+        "id": "cordova-plugin-audioinput.AudioInput",
+        "clobbers": [
+            "audioinput"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-audioinput": "0.3.0"
 };
 // BOTTOM OF METADATA
 });
--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,9 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-audioinput/www/audioInputCapture.js",
+        "id": "cordova-plugin-audioinput.AudioInput",
+        "pluginId": "cordova-plugin-audioinput",
+        "clobbers": [
+            "audioinput"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-audioinput": "0.3.0"
 }
 // BOTTOM OF METADATA
 });
